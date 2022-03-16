2238a913c8a6936eef5208e53f1d324224887877
Sasha Artsyman <65505084+Sasha-Artsyman@users.noreply.github.com>
2022-03-16 15:37:49 +0300

    Add files via upload

---
+++ Coin.cs
@@ -0,0 +1,18 @@
+﻿namespace CoinStatistic
+{
+    internal class Coin
+    {
+        int sideUp;
+        Random rnd = new Random();
+
+        public void toss()
+        {
+            // Get a random value, 0 or 1.
+            this.sideUp = rnd.Next(2);
+        }
+        public int getSideUp()
+        {
+            return sideUp;
+        }
+    }
+}
